2e9bcdce9c44fd001e3f8044fa6b83ce91b45c37
Dmitriy <damkovski@gmail.com>
2020-11-07 15:56:24 +0300

    added HW04.Task2

---
+++ HW04.Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HW04.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.task2();
+        }
+        private static void task2()
+        {
+        number1:
+            Console.WriteLine("Введите первое число");
+            string a = Console.ReadLine();
+            if (int.TryParse(a, out int a1))
+            {
+
+            }
+            else
+                goto number1;
+
+            number2:
+            Console.WriteLine("Введите второе число");
+            string b = Console.ReadLine();
+            if (int.TryParse(b, out int b1))
+            {
+
+            }
+            else
+                goto number2;
+
+            Console.WriteLine($"сумма чисел равняется {a1 + b1}");
+            Console.ReadKey();
+        }
+    }
+}
