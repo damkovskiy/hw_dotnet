180b11d81ab061b66fa578e33fc8d4571ab21207
Dmitriy <damkovski@gmail.com>
2020-11-07 16:58:06 +0300

    added HW04.Task3

--- HW04.Task2/Program.cs
+++ HW04.Task2/Program.cs
@@ -7,7 +7,22 @@ namespace HW04.Task2
     {
         static void Main(string[] args)
         {
-            Program.task2();
+            Console.WriteLine("Введите номер, интересующего вас метода(задания): 2 / 3");
+            string metod = Console.ReadLine();
+            int metod1 = int.Parse(metod);
+
+            switch (metod1)
+            {
+                case 2:
+                    Program.task2();
+                    break;
+                case 3:
+                    Program.task3();
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор - попробуйте снова");
+                    break;
+            }
         }
         private static void task2()
         {
@@ -21,7 +36,7 @@ namespace HW04.Task2
             else
                 goto number1;
 
-            number2:
+        number2:
             Console.WriteLine("Введите второе число");
             string b = Console.ReadLine();
             if (int.TryParse(b, out int b1))
@@ -32,6 +47,42 @@ namespace HW04.Task2
                 goto number2;
 
             Console.WriteLine($"сумма чисел равняется {a1 + b1}");
+            Console.ReadKey();
+        }
+        private static void task3()
+        {
+        number1:
+            Console.WriteLine("Введите первое число");
+            string a = Console.ReadLine();
+            if (int.TryParse(a, out int a1))
+            {
+
+            }
+            else
+                goto number1;
+
+        number2:
+            Console.WriteLine("Введите второе число");
+            string b = Console.ReadLine();
+            if (int.TryParse(b, out int b1))
+            {
+
+            }
+            else
+                goto number2;
+
+            Console.WriteLine($"Введите результат суммы");
+            string sum = Console.ReadLine();
+            int sum1 = int.Parse(sum);
+            int sum3 = a1 + b1;
+            if (sum1 == sum3)
+            {
+                Console.WriteLine($"Вы верно посчитали, молодец! Сумма равняется {sum3}");
+            }
+            else
+            {
+                Console.WriteLine("Ответ неверный!");
+            }
             Console.ReadKey();
         }
     }
