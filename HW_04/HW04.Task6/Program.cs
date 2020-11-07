ae292edaae210f75a46210fd8cafcab585dda1ef
Dmitriy <damkovski@gmail.com>
2020-11-07 20:33:49 +0300

    Added HW04.Task6

---
+++ HW_04/HW04.Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW04.Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 90;
+           
+            for (int i = 0; i < 26; i++)
+            {
+                int b = a--;
+                char c = Convert.ToChar(b);
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
