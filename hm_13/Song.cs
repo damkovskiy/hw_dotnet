fbcadf98e8e3a75bed84ca1987d9e6f6b7d40847
Dmitriy <damkovski@gmail.com>
2020-12-24 00:58:00 +0300

    added hm 13

---
+++ hm_13/Song.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_13
+{
+    class Song 
+    {
+        string _name;
+        int _duration;
+        string _author;
+        int _year;
+        public enum _genre 
+        {
+            pop,
+            hip_hop,
+            metal,
+            rock
+        };
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Duration { get { return _duration; } set { _duration = value; } }
+        public string Author { get { return _author; } set { _author = value; } }
+        public int Year { get { return _year; } set { _year = value; } }
+               
+        public void CreateSong()
+        {
+            Console.WriteLine("Input name of song");
+            Name = Console.ReadLine();
+            Console.WriteLine("Input duration of song");
+            Duration = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input author of song");
+            Author = Console.ReadLine();
+            Console.WriteLine("Input year of song");
+            Year = Convert.ToInt32(Console.ReadLine()); 
+        }
+    }
+}
